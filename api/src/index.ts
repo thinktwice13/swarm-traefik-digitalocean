809a8940318d7beaa5921cc66f873ebcea0d1894
thinktwice13 <marioo.krajacic@gmail.com>
2021-08-25 13:22:55 +0200

    Connect web and api: fetch data

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,13 @@
+import cors from 'cors'
 import express from 'express'
 
 const app = express()
+
+app.use(cors())
+
+app.get('/', (_, res) => {
+  res.status(200).json({ message: 'Hello from API' })
+})
 
 app.listen(4000, () => {
   console.log('API server running on port 4000.')
